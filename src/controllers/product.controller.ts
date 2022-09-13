0fe9d45235442c8e54f19aa7651270532f4870df
Cristiano Vieira <cristianoviieira@gmail.com>
2022-09-13 19:54:44 -0300

    feat: adiciona função da camada de controler que verifica certos dados e se comunica com a camada de service

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import ProductService from '../services/product.service';
+
+class ProductController {
+  constructor(private productsService = new ProductService()) { }
+
+  public getAll = async (_req: Request, res: Response) => {
+    const products = await this.productsService.getAll();
+    res.status(StatusCodes.OK).json(products);
+  };
+
+  public create = async (req: Request, res: Response) => {
+    const { body } = req;
+    console.log(body);
+    
+    const product = await this.productsService.create(body);
+    res.status(StatusCodes.CREATED).json(product);
+  };
+}
+
+export default ProductController;
