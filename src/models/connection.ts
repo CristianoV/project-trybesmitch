002a30b83739b9b1e3d77977509d7610184d71b3
Cristiano Vieira <cristianoviieira@gmail.com>
2022-09-13 21:11:23 -0300

    fix: concerta problemas com as variaveis de ambiente

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -3,10 +3,14 @@ import mysql from 'mysql2/promise';
 
 dotenv.config();
 
+const DB_HOSTNAME = process.env.HOST || 'db';
+const DB_USER = process.env.USER || 'root';
+const DB_PASSWORD = process.env.PASSWORD || 'password';
+
 const connection = mysql.createPool({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
+  host: DB_HOSTNAME,
+  user: DB_USER,
+  password: DB_PASSWORD,
 });
 
 export default connection;
