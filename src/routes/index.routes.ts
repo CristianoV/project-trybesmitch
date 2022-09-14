4a35ea89f06e05b864464489b8ab3e516b029494
Cristiano Vieira <cristianoviieira@gmail.com>
2022-09-14 16:48:46 -0300

    feat: adicionado novas rotas para pesquisa de usuarios

---
+++ src/routes/index.routes.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import ProductController from '../controllers/product.controller';
+import UserController from '../controllers/user.controller';
+
+const router = Router();
+
+const productController = new ProductController();
+const userController = new UserController();
+
+router.post('/products', productController.create);
+router.get('/products', productController.getAll);
+router.post('/users', userController.create);
+router.get('/users', userController.getAll);
+
+export default router;
